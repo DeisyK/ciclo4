9156d37ba1a1c25fbb39c38eb508b845fb365b35
Walter González <wg8901@hotmail.com>
2021-07-16 19:11:30 -0500

    fotos

--- agenda/src/Inicio.js
+++ agenda/src/Inicio.js
@@ -8,11 +8,11 @@ function Inicio() {
     const [integrantes, setIntegrantes] = useState([])
 
     useEffect(()=>{
-        setIntegrantes([ { foto:'https://i.ibb.co/KWChTk2/Whats-App-Image-2021-07-12-at-6-19-40-PM.jpg',  nombre: "Deisy Katherine Pineda", rol:"Scrum Master y gestor de proyectos", telefono: 3133523752,  Email: "kte_0330@hotmail.com"},
-        { foto:'https://i.ibb.co/KWChTk2/Whats-App-Image-2021-07-12-at-6-19-40-PM.jpg', nombre: "Nelson Ricardo Hurtado F.", rol:"DBA (Persistencia)", telefono: 3102215050, Email: "lf62@hotmail.com" },
-        { foto:'https://i.ibb.co/KWChTk2/Whats-App-Image-2021-07-12-at-6-19-40-PM.jpg', nombre: "Hector Jair Fonseca Pinto", rol:"Backend", telefono: 56932679167,  Email: "jairfonseca64@yahoo.es"},
-        {foto:'https://i.ibb.co/KWChTk2/Whats-App-Image-2021-07-12-at-6-19-40-PM.jpg', nombre: "Carlos Andrés Gutiérrez Cruz", rol:"Testing", telefono: 3153011375, Email: "contacto10@gmail.com"},
-        {foto:'https://i.ibb.co/KWChTk2/Whats-App-Image-2021-07-12-at-6-19-40-PM.jpg', nombre: "Walter Gonzalez Rincon", rol:"Frontend",telefono: 3215527911, Email: "wg8901@gmail.com" }])
+        setIntegrantes([ { foto:'https://i.ibb.co/qrcp2WS/fotodeisy1.jpg',  nombre: "Deisy Katherine Pineda", rol:"Scrum Master y gestor de proyectos", telefono: 3133523752,  Email: "kte_0330@hotmail.com"},
+        { foto:'https://i.ibb.co/0q5GZ45/fotonelson.jpg', nombre: "Nelson Ricardo Hurtado F.", rol:"DBA (Persistencia)", telefono: 3102215050, Email: "lf62@hotmail.com" },
+        { foto:'https://i.ibb.co/hsTQ8GQ/fotojair.png', nombre: "Hector Jair Fonseca Pinto", rol:"Backend", telefono: 56932679167,  Email: "jairfonseca64@yahoo.es"},
+        {foto:'https://i.ibb.co/f0tK9yZ/fotocarlos.png', nombre: "Carlos Andrés Gutiérrez Cruz", rol:"Testing", telefono: 3153011375, Email: "contacto10@gmail.com"},
+        {foto:'https://i.ibb.co/ngcpjvW/fotowalter1.jpg', nombre: "Walter Gonzalez Rincon", rol:"Frontend",telefono: 3215527911, Email: "wg8901@gmail.com" }])
     },[])    
 
     return (
@@ -38,7 +38,9 @@ function Inicio() {
             {integrantes.map((integrante,i)=>              
                 
                     <Card className='tarjetaPersonal' key={i}>
-                    <Card.Img variant="top" src={integrante.foto} />
+                    <Card.Img variant="top" src={integrante.foto}
+                                    width="100"
+                                    height="250"/>
                     <Card.Body>
                       <Card.Title>{integrante.nombre}</Card.Title>
                       <Card.Text>{integrante.rol}</Card.Text>            
