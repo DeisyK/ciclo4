4c96768207ace6720c5a5e00f0a95fd02a60ea4a
JairFonsecaP <jairfonseca64@yahoo.esa>
2021-07-22 18:55:06 -0500

    front

--- agenda/src/components/pages/Login.js
+++ agenda/src/components/pages/Login.js
@@ -1,49 +1,60 @@
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
-import { Button, Form, FormGroup, Label, Input, Card } from 'reactstrap';
-import '../css/login.css'
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { Button, Form, FormGroup, Label, Input, Card } from "reactstrap";
+import "../css/login.css";
 
 const Example = (props) => {
-  const [errorEmail, setErrorEmail]= useState(false)
-  const [errorPassword, setErrorPassword ]= useState(false)
+  const [errorEmail, setErrorEmail] = useState(false);
+  const [errorPassword, setErrorPassword] = useState(false);
 
-
-const validar = ()=>{
- 
-}
+  const validar = () => {
+    props.setLoginUser(true);
+  };
   return (
-      <Card className='login'>
-          
-    <Form className='formulario'>
-       <h4>Ingresa con E-mail y contraseña</h4>
-      <FormGroup>        
-        <Label for="exampleEmail">Email</Label>
-        <Input type="email" name="email" id="exampleEmail" placeholder="Ingresa tu E-mail" />
-      {errorEmail ? <p>Por favor ingrese un email valido</p>:null}
-      </FormGroup>
-      <FormGroup>
-        <Label for="examplePassword">Password</Label>
-        <Input type="password" name="password" id="examplePassword" placeholder="Ingresar Contraseña" />
-      </FormGroup>
-      <a   href="/RecuperarPw">Olvidaste tu contraseña</a>
-      
-     
-      <FormGroup check>
-        <Label check>
-          <Input type="checkbox" />{' '}
-          Recordarme
-        </Label>
-      </FormGroup>
-      <Link  to="/Miperfil" onClick={()=>{
-        validar()
-       
+    <Card className="login">
+      <Form className="formulario">
+        <h4>Ingresa con E-mail y contraseña</h4>
+        <FormGroup>
+          <Label for="exampleEmail">Email</Label>
+          <Input
+            type="email"
+            name="email"
+            id="exampleEmail"
+            placeholder="Ingresa tu E-mail"
+          />
+          {errorEmail ? <p>Por favor ingrese un email valido</p> : null}
+        </FormGroup>
+        <FormGroup>
+          <Label for="examplePassword">Password</Label>
+          <Input
+            type="password"
+            name="password"
+            id="examplePassword"
+            placeholder="Ingresar Contraseña"
+          />
+        </FormGroup>
+        <a href="/RecuperarPw">Olvidaste tu contraseña</a>
 
-      }}><Button >Entrar</Button></Link>
-      <Link to="/"><Button >← Volver</Button></Link>
-      <Link to="/Registro">Registrarse</Link>
-    </Form>
-      </Card>
+        <FormGroup check>
+          <Label check>
+            <Input type="checkbox" /> Recordarme
+          </Label>
+        </FormGroup>
+        <Link
+          to="/Miperfil"
+          onClick={() => {
+            validar();
+          }}
+        >
+          <Button>Entrar</Button>
+        </Link>
+        <Link to="/">
+          <Button>← Volver</Button>
+        </Link>
+        <Link to="/Registro">Registrarse</Link>
+      </Form>
+    </Card>
   );
-}
+};
 
 export default Example;
