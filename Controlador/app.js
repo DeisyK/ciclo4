ab2f2bd0005251f45f7e45c05870ca5823f15787
JairFonsecaP <jairfonseca64@yahoo.esa>
2021-07-25 15:31:52 -0500

    cambio de puerto

--- Controlador/app.js
+++ Controlador/app.js
@@ -3,7 +3,7 @@ const app = express();
 
 const router = require("./routes/index");
 
-const port = 3000;
+const port = 3030;
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
