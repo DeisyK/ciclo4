65954073e5c56e007054d7c3c21207ae06bde5cb
MTnhurtado <lf62@hotmail.com>
2021-07-30 19:12:06 -0400

    comm730

--- Modelo/config/mongoose/index.js
+++ Modelo/config/mongoose/index.js
@@ -1,6 +1,11 @@
 'use strict';
-const mongoose = require('mongoose');
+const { MongoClient } = require('mongodb');
 
-mongoose.connect('mongodb://localhost/auth');
+const uri = "mongodb+srv://MINTIC:grupo5@cluster0.tizvn.mongodb.net/BWN?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
-module.exports = mongoose;
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
