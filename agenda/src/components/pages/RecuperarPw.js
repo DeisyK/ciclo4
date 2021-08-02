4867d8d09d397ddd0e7044d7865b18c4157d3fef
JairFonsecaP <jairfonseca64@yahoo.esa>
2021-08-01 19:49:21 -0500

    Vistas de las categorias completas

--- agenda/src/components/pages/RecuperarPw.js
+++ agenda/src/components/pages/RecuperarPw.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Link, useHistory } from "react-router-dom";
+import { useHistory } from "react-router-dom";
 import { Form, Input, Button, Alert } from "antd";
 import { Card } from "reactstrap";
 import api from "../../assets/utils";
